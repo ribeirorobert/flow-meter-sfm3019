a0133c9fe19a7ad2a3b17f90dbd0fd9ea1e641ef
ribeirorobert <robertribeiro100@gmail.com>
2024-11-14 09:36:12 -0300

    tidal volume calculation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include "SFM3019.h"
 
+float tidalVolume = 0;
+unsigned long previousMillis;
+
 void setup() {
   Serial.begin(38400);
   Wire.begin();
@@ -11,6 +14,19 @@ void setup() {
 
 void loop() {
   if (SFMReadSensor(&FlowMeter.rawFlow) == INITIALIZED) {
-    Serial.println(FlowMeter.rawFlow);
+    
+    if (abs(FlowMeter.rawFlow) > 0.5) {
+      tidalVolume += ((FlowMeter.rawFlow / 60) * (millis() - previousMillis));
+      previousMillis = millis();
+
+      if (tidalVolume < 0) tidalVolume = 0;
+    } else {
+      tidalVolume = 0;
+    }
+
+    Serial.print(FlowMeter.rawFlow);
+    Serial.print('\t');
+    Serial.print(tidalVolume);
+    Serial.print('\n');
   }
 }
